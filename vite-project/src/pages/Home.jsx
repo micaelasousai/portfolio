e6658adf3a7f826041c59f569cee78aef72b7977
Micaela Sousa <118135686+micaelasousai@users.noreply.github.com>
2026-01-15 11:21:03 -0500

    navbar created

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,9 +1,16 @@
+
+import { NavBar } from "../components/NavBar";
+
 export const Home = () => {
     return <div className="min-h-screen">
 
         {/* NavBar */}
+        <NavBar />
 
         {/* Main Content */}
+        <main>
+            <HomeSection/>
+        </main>
 
         {/* Footer */}
 
